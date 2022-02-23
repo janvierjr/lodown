dccf082f0a4c6db43b22a9a888426d9f32586df2
Bernie January Jr <bernie@myheartsleeve.com>
2022-02-23 21:46:08 +0000

    lodown 3 complete

--- index.js
+++ index.js
@@ -9,7 +9,9 @@
  * @param {Array or Object} collection: The collection over which to iterate.
  * @param {Function} action: The Function to be applied to each value in the 
  * collection
+ * @returns 
  */
+
 function each(collection, action) {
     if(Array.isArray(collection)) {
         for(var i = 0; i < collection.length; i++) {
@@ -22,3 +24,39 @@ function each(collection, action) {
     }
 }
 module.exports.each = each;
+
+/**
+ * identity: Function takes in any value as an argument and returns that value unchanged.
+ * @param (any value) value: FUnciton takes in any value as an input value
+ * @returns (any value) value: Function returns the input values unchanged
+ */
+
+function identity(value) {
+    return value;
+}
+
+module.exports.identity = identity;
+
+/**
+ * typeof: function takes in any value as an argument and returns the data type of that value as a string
+ * @param {any value} value: function takes in any values as an input value 
+ * @returns (any value) value: function returns a string of the data type of the input value 
+ */
+
+function typeOf(value) {
+    if (typeof value !== "object") {
+        return typeof value;
+    } else if (Array.isArray(value)) {
+        return "array";
+    } else if (value === null) {
+        return "null";
+    } else if (value === undefined) {
+        return "undefined";
+    } 
+    return "object";
+
+ }
+
+ module.exports.typeOf = typeOf;
+
+ 
